39e12b7e898db35716c42cd78b2eee1250561e1c
Luke Murphy <lukewm@bitmessage.ch>
2014-10-10 21:12:13 -0200

    add port variable

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 var express = require('express');
+
 var app = express();
+var port = 3000;
 
 app.get('/', function(req, res){
       res.send('Hello World!');
 });
 
-var server = app.listen(3000, function() {
+var server = app.listen(port, function() {
         console.log('Listening on port %d', server.address().port);
 });
 
