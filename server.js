16dd400bd1c251ec26cafa585186ed73d597a96a
Luke Murphy <lukewm@bitmessage.ch>
2014-10-10 23:24:44 -0200

    add middleware calls - useful for many things like logging

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
+// requires
 var express    = require('express');
 var conf       = require('./app/config/conf.js');
 var mongoose   = require('mongoose');
 var bodyParser = require('body-parser');
 
-// schema imports
+// schema requires
 var User = require('./app/models/user.js');
 
-var app        = express();
-var router     = express.Router();
+var app    = express();
+var router = express.Router();
 
 app.set('port', conf.port);
 mongoose.connect(conf.mongodburl);
@@ -16,6 +17,15 @@ mongoose.connect(conf.mongodburl);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use('/api', router);
+
+// do something everytime API gets a request
+router.use(function(req, res, next) {
+    // do anything you want here
+    console.log('Received a request :)');
+
+    // continue on
+    next();
+});
 
 router.get('/', function(req, res) {
     res.json({ message: 'hooray! welcome to our api!' });
