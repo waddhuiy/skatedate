f33b716d8ac3474b73a175a8a777afb4c3a28c31
Luke Murphy <lukewm@bitmessage.ch>
2014-10-10 23:16:34 -0200

    add user schema import

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 var express    = require('express');
-var conf       = require('app/config/conf.js');
+var conf       = require('app/config/conf');
 var mongoose   = require('mongoose');
 var bodyParser = require('body-parser');
+
+// schema imports
+var User = require('app/models/user');
 
 var app        = express();
 var router     = express.Router();
@@ -9,12 +12,13 @@ var router     = express.Router();
 app.set('port', conf.port);
 mongoose.connect(conf.mongodburl);
 
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use('/api', router);
 
 router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });   
+    res.json({ message: 'hooray! welcome to our api!' });
 });
 
 
