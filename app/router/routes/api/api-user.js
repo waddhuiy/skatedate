8b86842a5446ac43796a001f31efd2a1d085a861
grady-lad <https://grady-lad@github.com>
2014-10-20 13:36:31 -0400

    Added the correct routing for user api, append router to 'post' or 'get'
    passing the '/' parameter specifies the spefic page. fixes #4

--- app/router/routes/api/api-user.js
+++ app/router/routes/api/api-user.js
@@ -2,9 +2,9 @@ var express = require('express');
 var User    = require('./../../../models/user.js');
 var router  = express.Router();
 
-router.route('/')
 
-    .post(function(req, res){
+
+   router.post('/' ,function(req, res){
         var user = new User();
 
         // @todo - grab from url
@@ -18,9 +18,12 @@ router.route('/')
         });
     })
 
-    .get(function(req, res){
+    router.get('/' ,function(req, res){
+        // res.json({message: "Users be using."});
         User.find(function(err, users){
             if(err){res.send(err);}
             res.json(users);
         });
     });
+
+module.exports = router;
